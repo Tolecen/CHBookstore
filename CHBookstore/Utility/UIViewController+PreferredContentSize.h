c519a93d4bb93c87d0a5dc033c18fabe84e94a95
lxy <lgjd33402@gmail.com>
2014-02-25 18:37:52 +0800

    add preferredcontentsize

---
+++ CHBookstore/Utility/UIViewController+PreferredContentSize.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+PreferredContentSize.h
+//  CHBookstore
+//
+//  Created by liuxiaoyu on 14-2-25.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (PreferredContentSize)
+
+@end
---
+++ CHBookstore/Utility/UIViewController+PreferredContentSize.m
@@ -0,0 +1,41 @@
+//
+//  UIViewController+PreferredContentSize.m
+//  CHBookstore
+//
+//  Created by liuxiaoyu on 14-2-25.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIViewController+PreferredContentSize.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (PreferredContentSize)
+
++ (void)load {
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		@autoreleasepool {
+			if (![self instanceMethodForSelector:@selector(preferredContentSize)]) {
+				class_addMethod(self,
+								@selector(preferredContentSize),
+								class_getMethodImplementation(self, @selector(bs_preferredContentSize)), method_getTypeEncoding(class_getInstanceMethod(self, @selector(bs_preferredContentSize))));
+			}
+			if (![self instanceMethodForSelector:@selector(setPreferredContentSize:)]) {
+				class_addMethod(self,
+								@selector(setPreferredContentSize:),
+								class_getMethodImplementation(self, @selector(bs_setPreferredContentSize:)),
+								method_getTypeEncoding(class_getInstanceMethod(self, @selector(bs_setPreferredContentSize:))));
+			}
+		}
+	});
+}
+
+- (void)bs_setPreferredContentSize:(CGSize)contentSize {
+    self.contentSizeForViewInPopover = contentSize;
+}
+
+- (CGSize)bs_preferredContentSize {
+    return self.contentSizeForViewInPopover;
+}
+
+@end
